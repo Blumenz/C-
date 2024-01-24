74dda3959df2a2c7b6b6d4068573feb4e295d2c0
Nicolas Ramis FT 27-C <100529262+Blumenz@users.noreply.github.com>
2024-01-24 15:08:01 -0300

    Add files via upload

---
+++ Project ArrayFinder/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string[] matrix = new string[8];
+
+        matrix[0] = "B123";
+        matrix[1] = "C234";
+        matrix[2] = "A345";
+        matrix[3] = "C15";
+        matrix[4] = "B177";
+        matrix[5] = "G3003";
+        matrix[6] = "C235";
+        matrix[7] = "B179";
+
+        foreach (string value in matrix)
+        {
+            if (value.StartsWith("B"))
+            {
+                Console.WriteLine(value);
+            }
+        }
+    }
+}
