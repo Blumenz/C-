e4d9b91a1ff12e769ee1c48a5fbd8f5c402e8499
Nicolas Ramis FT 27-C <100529262+Blumenz@users.noreply.github.com>
2024-01-24 15:11:28 -0300

    Add files via upload

---
+++ Project Boolean/Boolean Challenge/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.InteropServices;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Evaluation of conditionals with the random class
+        Random coin = new Random();
+        int flip = coin.Next(0, 2);
+        Console.WriteLine((flip == 0) ? "heads" : "tails");
+
+        string userPermission = "Admin";
+        int userLevel = 55;
+
+
+        Console.WriteLine((userLevel > 55) ? "Welcome, Super Admin user." : (userLevel <= 55 && userPermission == "Admin") ? "Welcome, Admin user." : (userLevel > 20 && userPermission == "Director") ? "Contact an Admin for access." : (userLevel < 20 && userPermission == "Director") ? "You do not have sufficient privileges." : "You do not have sufficient privileges.");
+    }
+}
---
+++ Project Boolean/ConsoleApp1/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.Reflection.Metadata;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        /*Use of == operator
+         * Console.WriteLine("a" == "a");
+        Console.WriteLine("a" == "A");
+        Console.WriteLine(1 == 2);
+
+        string myValue = "a";
+        Console.WriteLine(myValue == "a");*/
+
+        /*string value1 = " a";
+        string value2 = "A ";
+        Console.WriteLine(value1.Trim().ToLower() == value2.Trim().ToLower());*/
+
+        /*Use of != operator
+        Console.WriteLine("a" != "a");
+        Console.WriteLine("a" != "A");
+        Console.WriteLine(1 != 2);
+
+        string myValue = "a";
+        Console.WriteLine(myValue != "a");*/
+
+        //Use of >, <, >=, <= operators
+        /*Console.WriteLine(1 > 2);
+        Console.WriteLine(1 < 2);
+        Console.WriteLine(1 >= 1);
+        Console.WriteLine(1 <= 1);*/
+
+        //Boolean results
+        /*string pangram = "The quick brown fox jumps over the lazy dog.";
+        Console.WriteLine(pangram.Contains("fox"));
+        Console.WriteLine(pangram.Contains("cow"));*/
+        // These two lines of code will create the same output
+
+        /*Console.WriteLine(pangram.Contains("fox") == false);
+        Console.WriteLine(!pangram.Contains("fox"));*/
+
+        //Use of ! operator
+        /*string pangram = "The quick brown fox jumps over the lazy dog.";
+        Console.WriteLine(!pangram.Contains("fox"));
+        Console.WriteLine(!pangram.Contains("cow"));*/
+
+        //Use of != operator
+        /*int a = 7;
+        int b = 6;
+        Console.WriteLine(a != b); // output: True
+        string s1 = "Hello";
+        string s2 = "Hello";
+        Console.WriteLine(s1 != s2); // output: False*/
+
+        //Use of ? operator
+        int saleAmount = 1001;
+        /*int discount = saleAmount > 1000 ? 100 : 50;
+        Console.WriteLine($"Discount: {discount}");*/
+        Console.WriteLine($"Discount: {(saleAmount > 1000 ? 100 : 50)}");
+
+
+    }
+}
