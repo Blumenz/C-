25706863cf123e0613fef40a8d2f8a5e3618738a
NicolasRamis <100529262+Blumenz@users.noreply.github.com>
2024-01-24 15:57:52 -0300

    Add files via upload

---
+++ Project Remove()/MiniChallenge/Program.cs
@@ -0,0 +1,31 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        const string input = "<div><h2>Widgets &trade;</h2><span>5000</span></div>";
+
+        string quantity = "";
+        string output = "";
+
+        // Your work here
+
+        //code for quantity
+        const string openSpan = "<span>";
+        const string closeSpan = "</span>";
+        int openingPosition = input.IndexOf(openSpan);
+        int closingPosition = input.IndexOf(closeSpan);
+        openingPosition += openSpan.Length;
+        int length = closingPosition - openingPosition;
+        quantity = input.Substring(openingPosition, length);
+
+
+        //code for output
+        output = input;
+        output = output.Replace("&trade", "&reg");
+        output = output.Replace("<div>", "");
+        output = output.Replace("</div>", "");
+
+        Console.WriteLine("Quantity: "+ quantity);
+        Console.WriteLine(output);
+    }
+}
