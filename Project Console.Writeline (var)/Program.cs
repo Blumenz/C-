9ad0a200b21b1ffe635e319ff16a3cedcd7d3e37
Nicolas Ramis FT 27-C <100529262+Blumenz@users.noreply.github.com>
2024-01-24 15:25:14 -0300

    Add files via upload

---
+++ Project Console.Writeline (var)/Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //string firstName;
+        //firstName = "Bob";
+        //string firstName;
+        //"Bob" = firstName;
+        //int firstName;
+        //firstName = "Bob";
+        //string firstName;
+        //firstName = "Bob";
+        //Console.WriteLine(firstName);
+        string firstName;
+        firstName = "Bob";
+        Console.WriteLine(firstName);
+        firstName = "Liem";
+        Console.WriteLine(firstName);
+        firstName = "Isabella";
+        Console.WriteLine(firstName);
+        firstName = "Yasmin";
+        Console.WriteLine(firstName);
+    }
+}
+
