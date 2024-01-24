120a2195d8d68ec68c3b1c230f6ac002389b346b
Nicolas Ramis FT 27-C <100529262+Blumenz@users.noreply.github.com>
2024-01-24 15:24:26 -0300

    Add files via upload

---
+++ Project Console.Writeline-/Program.cs
@@ -0,0 +1,26 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine('b');
+        //Console.WriteLine('Hello World!');
+        Console.WriteLine(123);
+        Console.WriteLine(0.25F);
+        Console.WriteLine(2.625);
+        Console.WriteLine(12.39816m);
+        Console.WriteLine(true);
+        Console.WriteLine(false);
+        Console.WriteLine("123");
+        Console.WriteLine(123);
+        Console.WriteLine("true");
+        Console.WriteLine(true);
+        /*
+          string para palabras, frases o cualquier dato alfanumérico para presentación, no cálculo
+          char para un solo carácter alfanumérico
+          int para un número entero
+          decimal para un número con un componente fraccionado
+          bool para un valor true/false
+        */
+    }
+}
+
