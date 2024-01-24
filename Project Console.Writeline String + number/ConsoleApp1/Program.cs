082bdf9d1a89464f437e2c7dceea11f7daf8c914
Nicolas Ramis FT 27-C <100529262+Blumenz@users.noreply.github.com>
2024-01-24 15:27:17 -0300

    Add files via upload

---
+++ Project Console.Writeline String + number/ConsoleApp1/Program.cs
@@ -0,0 +1,64 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        /*int sum = 7 + 5;
+        int difference = 7 - 5;
+        int product = 7 * 5;
+        int quotient = 7 / 5;
+
+        Console.WriteLine("Sum: " + sum);
+        Console.WriteLine("Difference: " + difference);
+        Console.WriteLine("Product: " + product);
+        Console.WriteLine("Quotient: " + quotient);
+
+        decimal decimalQuotient = 7.0m / 5;
+        Console.WriteLine($"Decimal quotient: {decimalQuotient}");*/
+
+        /*int first = 7;
+        int second = 5;
+        decimal quotient = (decimal)first / (decimal)second;
+        Console.WriteLine(quotient);*/
+
+        /*Console.WriteLine($"Modulus of 200 / 5 : {200 % 5}");
+        Console.WriteLine($"Modulus of 7 / 5 : {7 % 5}");*/
+
+        /*int value = 1;
+
+        value = value + 1;
+        Console.WriteLine("First increment: " + value);
+
+        value += 1;
+        Console.WriteLine("Second increment: " + value);
+
+        value++;
+        Console.WriteLine("Third increment: " + value);
+
+        value = value - 1;
+        Console.WriteLine("First decrement: " + value);
+
+        value -= 1;
+        Console.WriteLine("Second decrement: " + value);
+
+        value--;
+        Console.WriteLine("Third decrement: " + value);*/
+
+
+        /*int value = 1;
+        value++;
+        Console.WriteLine("First: " + value);
+        Console.WriteLine($"Second: {value++}");
+        Console.WriteLine("Third: " + value);
+        Console.WriteLine("Fourth: " + (++value));*/
+
+        /*int fahrenheit = 94;
+        decimal Celsius = ((fahrenheit - 32) * (5 / 9m));
+        Console.WriteLine("The temperature is " + Celsius);*/
+
+        Console.WriteLine(5 / 10);
+
+
+    }
+
+
+}
