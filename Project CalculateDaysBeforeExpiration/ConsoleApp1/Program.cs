708fe4fcc45260e64380613c61e9a30ec6685b33
Nicolas Ramis FT 27-C <100529262+Blumenz@users.noreply.github.com>
2024-01-24 15:14:23 -0300

    Add files via upload

---
+++ Project CalculateDaysBeforeExpiration/ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Random random = new Random();
+        int daysUntilExpiration = random.Next(12);
+        int discountPercentage = 0;
+
+        // Your code goes here
+
+        if (daysUntilExpiration <= 10 && daysUntilExpiration > 5)
+        {
+            Console.WriteLine("Your subscription will expire soon. Renew now!");
+        }
+        else if (daysUntilExpiration <= 5 && daysUntilExpiration >1)
+        {
+            Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.");
+            discountPercentage = 10;
+        }
+        else if(daysUntilExpiration == 1 )
+        {
+            Console.WriteLine($"Your subscription expires within a day!");
+            discountPercentage = 20;
+        }
+        else if(daysUntilExpiration == 0)
+        {
+            Console.WriteLine("Your subscription has expired");
+        }
+        else
+        {
+            Console.WriteLine();
+        }
+        if(discountPercentage > 0)
+        {
+            Console.WriteLine($"Renew now and save {discountPercentage}%");
+        }
+    }
+}
