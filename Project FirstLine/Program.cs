e739c2da3cb101f63dea5dc30e06864d6349dcca
Nicolas Ramis FT 27-C <100529262+Blumenz@users.noreply.github.com>
2024-01-24 15:39:23 -0300

    Add files via upload

---
+++ Project FirstLine/Program.cs
@@ -0,0 +1,12 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Console.WriteLine("Hello World!");
+        Console.Write("Congratulation!");
+        Console.Write(" ");
+        Console.Write("You wrote your first lines of code.");
+        // Console.Write permite escribir en la misma linea mientrasque Writeline genera un salto de linea
+    }
+}
+
